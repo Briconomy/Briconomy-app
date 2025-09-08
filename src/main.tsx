fb930a3a24ba1bc3ab546aed82dc23749d8602e0
Max0xDay <backup.day28@gmail.com>
2025-09-08 07:21:32 -0400

    _setLoading

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
+import App from './App.tsx';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 import './styles/global.css';
 import './utils/chart-registration.ts';
 
